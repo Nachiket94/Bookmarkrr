bcb4fdb8214f23b2055ba856653b790841048f7e
Nachu <nachi19229@gmail.com>
2023-06-19 15:41:12 +0530

    Added function to save button and overlay

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ const closeBkmModal = document.querySelector(".closeBkm");
 const closeGrpModal = document.querySelector(".closeGrp");
 const new_grp_modal = document.querySelector(".new_group_modal");
 const new_items_modal = document.querySelector(".new_items_modal");
+const new_bkm_name = document.getElementById("bkm_name");
+const new_bkm_url = document.getElementById("url");
+const new_grp_name = document.getElementById("group_name");
+const new_items_submit = document.getElementById('new_items_submit');
 
+const bookmarks = [];
 
+const addBookmark = (name, url, groupName) =>{
+    bookmarks.push({ 'name': name,
+        'url' : url, 
+        'groupName' : groupName
+    });
+};
 
+let grp = "adsf"
 // Functions Declaration 
 function openModal(e){
     e.classList.remove("hidden");
@@ -19,6 +31,16 @@ function closeModal(e){
     overlay.classList.add("hidden");
     console.log("Modal Closed")
 }
+
+new_items_submit.addEventListener('click', ()=>{
+    if(new_bkm_name.value != null && new_bkm_url.value != null){
+        addBookmark(new_bkm_name, new_bkm_url, grp);
+        localStorage.setItem('data', bookmarks);
+    }
+    closeModal(new_items_modal)
+});
+
+
 // Open and close New bookmarks modal
 new_bookmark_btn.addEventListener('click',()=>{openModal(new_items_modal)})
 closeBkmModal.addEventListener('click',()=>{closeModal(new_items_modal)});
@@ -33,3 +55,4 @@ document.addEventListener("keydown", function (e) {
         closeModal(new_grp_modal);
     }
 });
+
